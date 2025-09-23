2b641b7d505fec638b6edb3705748a9f6b2fc29d
JhonGalindez <jhongalindez3@gmail.com>
2025-09-23 18:59:56 -0500

    Tercer commit JB

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -202,20 +202,24 @@
 	    });
 	});
 
-	function onScroll(event){
-	    var scrollPos = $(document).scrollTop();
-	    $('.nav a').each(function () {
-	        var currLink = $(this);
-	        var refElement = $(currLink.attr("href"));
-	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-	            $('.nav ul li a').removeClass("active");
-	            currLink.addClass("active");
-	        }
-	        else{
-	            currLink.removeClass("active");
-	        }
-	    });
-	}
+	function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('.nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        
+        // Verificar que exista el elemento antes de usarlo
+        if (refElement.length) {
+            if (refElement.position().top <= scrollPos &&
+                refElement.position().top + refElement.height() > scrollPos) {
+                $('.nav ul li a').removeClass("active");
+                currLink.addClass("active");
+            } else {
+                currLink.removeClass("active");
+            }
+        }
+    });
+}
 
 
 	// Page loading animation
