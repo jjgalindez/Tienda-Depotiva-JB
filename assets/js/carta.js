e2eaf8dc9b58273cf045858fcebbdda4c662ca8f
Jhon Janer Galindez <jhongalindez3@gmail.com>
2025-09-25 18:06:31 +0000

    Doce commit JB

--- assets/js/carta.js
+++ assets/js/carta.js
@@ -4,15 +4,29 @@ document.getElementById("formCarta").addEventListener("submit", function(e) {
 
   // Datos del formulario
   const nombre = document.getElementById("nombre").value;
+  const fecha = document.getElementById("fecha").value;
   //const telefono = document.getElementById("telefono").value;
 
-  const carta = `Hola, soy ${nombre}, quiero invitarte a Tienda Deportiva JB, venden guayos de 
+  const carta = `
+  Fecha de invitación: ${fecha}
+
+  Hola, soy ${nombre}, quiero invitarte a Tienda Deportiva JB donde venden guayos de 
   excelente calidad. 
-  Ofrecen varios modelos disponibles y promociones especiales. 
+  Ofrecen los ultimos modelos disponibles en el mercado y ademas promociones especiales. 
   Escríbeles  para más detalles. WhatsApp 3137096423`;
+
 
   
   const url = `https://wa.me/?text=${encodeURIComponent(carta)}`;
 
   window.open(url, "_blank");
+
+  const { jsPDF } = window.jspdf; // si usas el script desde CDN
+  const doc = new jsPDF();
+
+  doc.setFont("helvetica", "normal");
+  doc.setFontSize(12);
+  doc.text(carta, 10, 20); // (texto, x, y)
+
+  doc.save("carta-invitacion.pdf"); // descarga automática
 });
