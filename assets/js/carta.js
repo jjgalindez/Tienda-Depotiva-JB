77724c9b059ef762b252c1d495e6b8cc23771bef
Jhon Janer Galindez <jhongalindez3@gmail.com>
2025-09-25 17:52:13 +0000

    Revert "Once commit JB"
    This reverts commit bdf2ff5165b64d52a7bbe4a2b207c302844086ec.

--- assets/js/carta.js
+++ assets/js/carta.js
@@ -1,26 +1,18 @@
-import { jsPDF } from "jspdf";
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("formCarta").addEventListener("submit", function (e) {
-    e.preventDefault();
+document.getElementById("formCarta").addEventListener("submit", function(e) {
+  e.preventDefault();
 
-    const nombre = document.getElementById("nombre").value;
+  // Datos del formulario
+  const nombre = document.getElementById("nombre").value;
+  //const telefono = document.getElementById("telefono").value;
 
-    const carta = `Hola, soy ${nombre}, quiero invitarte a Tienda Deportiva JB, venden calzado deportivo de 
-      excelente calidad para mejorar nuestro rendimiento en las canchas como deportistas. 
-      Ofrecen varios modelos disponibles y promociones especiales. 
-      Escríbeles para más detalles. WhatsApp 3137096423`;
+  const carta = `Hola, soy ${nombre}, quiero invitarte a Tienda Deportiva JB, venden guayos de 
+  excelente calidad. 
+  Ofrecen varios modelos disponibles y promociones especiales. 
+  Escríbeles  para más detalles. WhatsApp 3137096423`;
 
-    // --- nviar a WhatsApp ---
-    const url = `https://wa.me/?text=${encodeURIComponent(carta)}`;
-    window.open(url, "_blank");
+  
+  const url = `https://wa.me/?text=${encodeURIComponent(carta)}`;
 
-    // --- Generar PDF ---
-    const { jsPDF } = window.jspdf;
-    const doc = new jsPDF();
-    doc.setFont("helvetica", "normal");
-    doc.setFontSize(12);
-    doc.text(carta, 10, 20);
-    doc.save("carta-invitacion.pdf");
-  });
+  window.open(url, "_blank");
 });
